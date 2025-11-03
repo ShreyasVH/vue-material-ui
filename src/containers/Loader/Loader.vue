e6b4f0594a785a54504930ee99739f1fef9a54f7
ShreyasVH <shreyas.deve.1@gmail.com>
2025-11-03 23:03:49 +0530

    loader support

---
+++ src/containers/Loader/Loader.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-btn color="primary" v-on:click="showLoader">
+    Show Loader
+  </v-btn>
+
+  <div v-if="visible" v-on:click="hideLoader">
+    <v-dialog v-model="visible" width="100" transition="fade-transition">
+      <v-card class="d-flex justify-center align-center" height="100" style="background-color: transparent; box-shadow: none;">
+        <v-progress-circular indeterminate color="primary" size="48" />
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Loader",
+  data () {
+    return {
+      visible: false
+    }
+  },
+  mounted() {
+    const that = this
+    window.addEventListener('show-loader', function(event) {
+      that.setLoaderValue(true)
+    });
+
+    window.addEventListener('hide-loader', function(event) {
+      that.setLoaderValue(false)
+    });
+  },
+  methods: {
+    showLoader: function (event) {
+      event.preventDefault();
+
+      const myEvent = new CustomEvent('show-loader', {});
+      window.dispatchEvent(myEvent);
+    },
+
+    hideLoader: function (event) {
+      event.preventDefault();
+
+      const myEvent = new CustomEvent('hide-loader', {});
+      window.dispatchEvent(myEvent);
+    },
+
+    setLoaderValue: function (value) {
+      this.visible = value
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
