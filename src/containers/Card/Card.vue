d9f6158e60c1067d8e5e8bd4589b0b1f8f714f84
ShreyasVH <shreyas.deve.1@gmail.com>
2026-02-09 20:00:06 +0530

    support for cricket card

--- src/containers/Card/Card.vue
+++ src/containers/Card/Card.vue
@@ -53,6 +53,100 @@
       </v-expand-transition>
     </v-card>
 
+    <br />
+
+    <v-card
+        class="mx-auto"
+        max-width="300"
+        style="text-align: center"
+    >
+      <v-card-text>
+        <p class="text-h4 font-weight-black">Jack Kallis</p>
+
+        <p>South Africa</p>
+
+        <img src="https://res.cloudinary.com/dyoxubvbg/image/upload/v1577106216/artists/default_m.jpg" />
+
+        <div class="text-medium-emphasis" style="text-align: left">
+          <v-row no-gutters>
+            <v-col md="6">
+              <p>
+                <strong>Innings:</strong>
+                25
+              </p>
+
+              <p>
+                <strong>Runs:</strong>
+                25786
+              </p>
+
+              <p>
+                <strong>Balls:</strong>
+                25
+              </p>
+
+              <p>
+                <strong>Average:</strong>
+                45.46
+              </p>
+
+              <p>
+                <strong>Not Outs:</strong>
+                72
+              </p>
+
+              <p>
+                <strong>Highest: </strong>
+                172
+              </p>
+
+              <p>
+                <strong>50/100: </strong>
+                25/10
+              </p>
+            </v-col>
+
+            <v-col md="6">
+              <p>
+                <strong>Catches</strong>
+                123
+              </p>
+
+              <p>
+                <strong>Wickets:</strong>
+                123
+              </p>
+
+              <p>
+                <strong>Balls: </strong>
+                12302
+              </p>
+
+              <p>
+                <strong>Runs: </strong>
+                12302
+              </p>
+
+              <p>
+                <strong>Fifers: </strong>
+                12
+              </p>
+
+              <p>
+                <strong>Economy: </strong>
+                7.85
+              </p>
+
+              <p>
+                <strong>DOB: </strong>
+                24/01/1967
+              </p>
+            </v-col>
+          </v-row>
+        </div>
+      </v-card-text>
+    </v-card>
+
   </div>
 
 </template>
