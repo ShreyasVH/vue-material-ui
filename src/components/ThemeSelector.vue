131754359e811ffc7be62822123a9e07146cea83
ShreyasVH <shreyas.deve.1@gmail.com>
2026-08-14 19:29:20 +0530

    separating out theme selector

---
+++ src/components/ThemeSelector.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-switch
+      color="secondary"
+      :label="mode === 'dark' ? 'Dark mode' : 'Light mode'"
+      @change="toggleMode"
+      v-model="isDark"
+  ></v-switch>
+</template>
+
+<script>
+  import { useTheme } from 'vuetify'
+
+  export default {
+    name: "ThemeSelector",
+    setup() {
+      const theme = useTheme();
+
+      return {
+        theme
+      }
+    },
+    data () {
+      const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+      return {
+        mode: prefersDark ? 'dark' : 'light'
+      }
+    },
+    methods: {
+      toggleMode () {
+        const newMode = this.mode === 'dark' ? 'light' : 'dark';
+        this.mode = newMode;
+        this.theme.change(newMode);
+      }
+    },
+    computed: {
+      isDark () {
+        return this.mode === 'dark';
+      }
+    }
+  }
+</script>
